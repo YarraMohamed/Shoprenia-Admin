351677899cddb0fb984e067403f96318f88a087c
Abdullah Raed <abdullahraediti@gmail.com>
2025-05-27 20:47:21 +0300

    firist commit

---
+++ Shoprenia-Admin/Shoprenia_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoprenia_AdminApp.swift
+//  Shoprenia-Admin
+//
+//  Created by Abdullah Raed on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Shoprenia_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
